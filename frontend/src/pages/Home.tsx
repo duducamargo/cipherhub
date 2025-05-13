1e427a70e146f8f4d4bb288b9d4ce569cd807cf6
Eduardo Farias Camargo <edu.farias.camargo@gmail.com>
2025-05-13 19:39:25 -0300

    [add] tabs ui has been added

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,12 +2,15 @@
 
 import HomeTitle from "../components/HomeTitle";
 import CanvasRevealHome from "../components/CanvasRevealHome";
+import { TabsHome } from "@/components/TabsHome";
 
 export default function App() {
   return (
     <div className="bg-[#111] text-white w-full min-h-screen flex flex-col items-center justify-center">
 
       <HomeTitle />
+
+      <TabsHome />
       
       <CanvasRevealHome />
     </div>
