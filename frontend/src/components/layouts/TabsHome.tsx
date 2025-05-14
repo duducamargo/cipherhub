9b47adfa6b5d4b373af4d1e4927d011bd0089c4e
Eduardo Farias Camargo <edu.farias.camargo@gmail.com>
2025-05-14 16:56:10 -0300

    [add] home tab symetric encryption has been added

--- frontend/src/components/layouts/TabsHome.tsx
+++ frontend/src/components/layouts/TabsHome.tsx
@@ -4,6 +4,7 @@ import { Tabs } from "../ui/tabs";
 import { TabDefinition } from "../TabDefinition";
 import { TabHistory } from "../TabHistory";
 import { TabSymetricEncryption } from "../TabSymmetricEncryption";
+import { TabAssymetricEncryption } from "../TabAssymmetricEncryption";
 
 export function TabsHome() {
   const tabs = [
@@ -39,7 +40,7 @@ export function TabsHome() {
       value: "criptografia-assimetrica",
       content: (
         <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-gradient-to-br from-purple-600 to-violet-900">
-          <TabDefinition />
+          <TabAssymetricEncryption />
         </div>
       ),
     },
@@ -47,7 +48,7 @@ export function TabsHome() {
   ];
 
   return (
-    <div className="h-[180rem] md:h-[210rem] [perspective:1000px] relative b flex flex-col max-w-5xl mx-auto w-full  items-start justify-start my-24">
+    <div className="h-[225rem] md:h-[210rem] [perspective:1000px] relative b flex flex-col max-w-5xl mx-auto w-full  items-start justify-start my-24">
       <Tabs tabs={tabs} />
     </div>
   );
