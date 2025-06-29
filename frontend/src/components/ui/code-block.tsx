e281bbc5eccfcf8b5ab23d83f9d7403868428936
Eduardo Farias Camargo <edu.farias.camargo@gmail.com>
2025-06-29 20:13:28 -0300

    style(code-block): update theme and background for visual consistency

--- frontend/src/components/ui/code-block.tsx
+++ frontend/src/components/ui/code-block.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import { IconCheck, IconCopy } from "@tabler/icons-react";
 
 type CodeBlockProps = {
@@ -54,7 +54,7 @@ export const CodeBlock = ({
     : highlightLines;
 
   return (
-    <div className="relative w-[95%] sm:w-[83%] max-h-[500px] overflow-auto rounded-lg bg-slate-900 p-4 font-mono text-sm">
+    <div className="relative w-[95%] sm:w-[83%] max-h-[500px] overflow-auto rounded-lg bg-zinc-900 p-4 font-mono text-sm">
       <div className="flex flex-col gap-2">
         {tabsExist && (
           <div className="flex  overflow-x-auto">
@@ -87,7 +87,7 @@ export const CodeBlock = ({
       </div>
       <SyntaxHighlighter
         language={activeLanguage}
-        style={atomDark}
+        style={dracula}
         customStyle={{
           margin: 0,
           padding: 0,
